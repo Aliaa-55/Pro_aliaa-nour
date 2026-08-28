9283e8881b7e5b940ccc4b86e2f6df8627bcee56
aliaa <aliaa0110050020@gmail.com>
2026-08-28 11:34:55 +0300

    nour friday 11:00 pa

---
+++ aliaa_nour.js
@@ -0,0 +1,8 @@
+function changeText() {
+    document.getElementById("title").innerHTML = "Welcome 🎉";
+
+    document.getElementById("text").innerHTML =
+        "You clicked the button!";
+
+    document.getElementById("title").style.color = "blue";
+}
